3c9f4c91d0f4c54ade2671757ef7027ff2910e76
Recep Taha Konyar <112318821+CodeTaha@users.noreply.github.com>
2024-05-23 17:01:06 +0300

    Update Kutuphane_Veri_Tabani.py
    new features update

--- Kutuphane_Veri_Tabani.py
+++ Kutuphane_Veri_Tabani.py
@@ -1,13 +1,22 @@
 import sqlite3
 import time
 
+
 class Kullanici:
-    def __init__(self, ad, soyad, email, kullanici_adi, sifre):
+    def __init__(self, ad, soyad, email, kullanici_adi, sifre, guvenlik_sorusu, cinsiyet):
         self.ad = ad
         self.soyad = soyad
         self.email = email
         self.kullanici_adi = kullanici_adi
         self.sifre = sifre
+        self.guvenlik_sorusu = guvenlik_sorusu
+        self.cinsiyet = cinsiyet
+
+    def email_dogrulama(self):
+        if "@gmail.com" in self.email:
+            return True
+        else:
+            return False
 
     def bilgileri_goster(self):
         print("\n Kullanıcı Bilgileri: ")
@@ -16,13 +25,23 @@ class Kullanici:
             Soyad: {self.soyad}
             Email: {self.email}
             Kullanıcı Adı: {self.kullanici_adi}
+            Cinsiyet :{self.cinsiyet}
             Şifre: ********
+            Güvenlik sorusu: **
         """)
 
     def bilgileri_guncelle(self):
         yeni_ad = input("Kullanıcının Yeni adını giriniz (boş bırakırsanız eski ad kaydedilmiş olacaktır): ")
         yeni_soyad = input("Kullanıcının Yeni soyadını giriniz (boş bırakırsanız eski soyad kaydedilmiş olacaktır): ")
-        yeni_email = input("Kullanıcının Yeni emailini giriniz (boş bırakırsanız eski email kaydedilmiş olacaktır): ")
+        while True:
+            yeni_email = input(
+                "Kullanıcının Yeni emailini giriniz (boş bırakırsanız eski email kaydedilmiş olacaktır): ")
+            if yeni_email:
+                if "@gmail.com" in yeni_email:
+                    self.email = yeni_email
+                    break
+                else:
+                    print("Mail Adresini kullanıma uygun giriniz (örn; 12345@gmail.com)")
         yeni_kullanici_adi = input(
             "Kullanıcının Yeni kullanıcı adını giriniz (boş bırakırsanız eski kullanıcı ad kaydedilmiş olacaktır): ")
         yeni_sifre = input("Kullanıcının Yeni şifresini giriniz (boş bırakırsanız eski şifre kaydedilmiş olacaktır): ")
@@ -38,6 +57,7 @@ class Kullanici:
         if yeni_sifre:
             self.sifre = yeni_sifre
         print("Kullanıcı bilgileri başarılı bir şekilde güncellendi.")
+
 
 class Yonetmen:
     def __init__(self, yonetmen_ad_soyad, dogum_yili):
@@ -65,11 +85,12 @@ class Yonetmen:
 
 
 class Film:
-    def __init__(self, film_adi, yonetmen_ad_soyad, cikis_yili, tur):
+    def __init__(self, film_adi, yonetmen_ad_soyad, cikis_yili, tur, sure):
         self.film_adi = film_adi
         self.yonetmen_ad_soyad = yonetmen_ad_soyad
         self.cikis_yili = cikis_yili
         self.tur = tur
+        self.sure = sure
 
     def bilgileri_goster(self):
         print("\nFilm Bilgileri: ")
@@ -78,6 +99,7 @@ class Film:
             Yönetmen Adı ve Soyadı: {self.yonetmen_ad_soyad}
             Çıkış Yılı: {self.cikis_yili}
             Tür: {self.tur}
+            Süre (dk.): {self.sure}
         """)
 
     def bilgileri_guncelle(self):
@@ -86,14 +108,16 @@ class Film:
             "Filmin Yeni yönetmeninin ad ve soyadını giriniz (boş bırakırsanız eski yönetmen kaydedilmiş olacaktır): ")
         yeni_cikis_yili = input(
             "Filmin Yeni çıkış yılını giriniz (boş bırakırsanız eski çıkış yılı kaydedilmiş olacaktır): ")
-        yeni_tur = input("Filmin Yeni türünü giriniz (boş bırakırsanız eski türü kaydedilmiş olacaktır): ")
+        yeni_tur = input("Filmin yeni türünü giriniz (boş bırakırsanız eski türü kaydedilmiş olacaktır): ")
+        yeni_sure = input("Filmin yeni süresini giriniz (boş bırakırsanız eski türü kaydedilmiş olacaktır): ")
 
         if yeni_film_adi:
             film = film_kutuphanesi.film_aramak(yeni_film_adi)
             if film:
                 self.film_adi = yeni_film_adi
             else:
-                print("Girmiş olduğunuz film bilgileri bulunamadı. Lütfen öncelikel filmi sisteme ekleyin daha sonrasında flm güncellemesi yapınız.")
+                print(
+                    "Girmiş olduğunuz film bilgileri bulunamadı. Lütfen öncelikel filmi sisteme ekleyin daha sonrasında flm güncellemesi yapınız.")
         if yeni_yonetmen_ad_soyad:
             yonetmen = film_kutuphanesi.yonetmen_aramak(yeni_yonetmen_ad_soyad)
             if yonetmen:
@@ -106,6 +130,9 @@ class Film:
         if yeni_tur:
             self.tur = yeni_tur
         print("Film bilgileri başarılı bir şekilde güncellendi.")
+
+        if yeni_sure:
+            self.sure = yeni_sure
 
 
 class FilmKutuphanesi:
@@ -123,7 +150,9 @@ class FilmKutuphanesi:
                 soyad VARCHAR(100),
                 email VARCHAR(100),
                 kullanici_adi VARCHAR(100),
-                sifre VARCHAR(50)
+                sifre VARCHAR(50),
+                guvenlik_sorusu VARCHAR(2),
+                cinsiyet VARCHAR(10)
             )
         ''')
 
@@ -142,6 +171,7 @@ class FilmKutuphanesi:
                 yonetmen_ad_soyad VARCHAR(200),
                 cikis_yili INTEGER,
                 tur VARCHAR(100),
+                sure INTEGER,
                 FOREIGN KEY (yonetmen_ad_soyad) REFERENCES Yonetmen(ad_soyad)
             )
         ''')
@@ -149,12 +179,16 @@ class FilmKutuphanesi:
         self.connect.commit()
 
     def kullanici_ekle(self, kullanici):
-        self.cursor.execute('''
-            INSERT INTO Kullanici (ad, soyad, email, kullanici_adi, sifre)
-            VALUES (?, ?, ?, ?, ?)
-        ''', (kullanici.ad, kullanici.soyad, kullanici.email, kullanici.kullanici_adi, kullanici.sifre))
-        self.connect.commit()
-        print(f" {kullanici.kullanici_adi} başarılı bir şekilde eklendi.")
+        if kullanici.email_dogrulama():
+            self.cursor.execute('''
+                        INSERT INTO Kullanici (ad, soyad, email, kullanici_adi, sifre, guvenlik_sorusu,cinsiyet)
+                        VALUES (?, ?, ?, ?, ?, ?, ?)
+                    ''', (kullanici.ad, kullanici.soyad, kullanici.email, kullanici.kullanici_adi, kullanici.sifre,
+                          kullanici.guvenlik_sorusu, kullanici.cinsiyet))
+            self.connect.commit()
+            print(f" {kullanici.kullanici_adi} başarılı bir şekilde eklendi.")
+        else:
+            print("Mail Adresini kullanıma uygun giriniz (örn; 12345@gmail.com)")
 
     def yonetmen_ekle(self, yonetmen):
         self.cursor.execute('''
@@ -166,16 +200,16 @@ class FilmKutuphanesi:
 
     def film_ekle(self, film):
         self.cursor.execute('''
-            INSERT INTO Film (film_adi, yonetmen_ad_soyad, cikis_yili, tur)
-            VALUES (?, ?, ?, ?)
-        ''', (film.film_adi, film.yonetmen_ad_soyad, film.cikis_yili, film.tur))
+            INSERT INTO Film (film_adi, yonetmen_ad_soyad, cikis_yili, tur, sure)
+            VALUES (?, ?, ?, ?, ?)
+        ''', (film.film_adi, film.yonetmen_ad_soyad, film.cikis_yili, film.tur, film.sure))
         self.connect.commit()
         print(f"{film.film_adi} başarılı bir şekilde eklendi.")
 
     def kullanicilari_goster(self):
         rows = self.cursor.execute('SELECT * FROM Kullanici').fetchall()
         for row in rows:
-            kullanici = Kullanici(row[1], row[2], row[3], row[4], row[5])
+            kullanici = Kullanici(row[1], row[2], row[3], row[4], row[5], row[6], row[7])
             kullanici.bilgileri_goster()
 
     def kul_adi_sifre(self, kullanici_adi, sifre):
@@ -197,7 +231,7 @@ class FilmKutuphanesi:
     def filmleri_goster(self):
         rows = self.cursor.execute('SELECT * FROM Film').fetchall()
         for row in rows:
-            film = Film(row[1], row[2], row[3], row[4])
+            film = Film(row[1], row[2], row[3], row[4], row[5])
             film.bilgileri_goster()
 
     def kullanici_sil(self, kullanici_adi):
@@ -218,51 +252,61 @@ class FilmKutuphanesi:
     def film_aramak(self, film_adi):
         row = self.cursor.execute('SELECT * FROM Film WHERE LOWER(film_adi) LIKE ?', (f'%{film_adi}%',)).fetchone()
         if row:
-            return Film(row[1], row[2], row[3], row[4])
+            return Film(row[1], row[2], row[3], row[4], row[5])
         return None
 
     def yonetmen_aramak(self, yonetmen_ad_soyad):
-        row = self.cursor.execute('SELECT * FROM Yonetmen WHERE LOWER(ad_soyad) LIKE ?', (f'%{yonetmen_ad_soyad}%',)).fetchone()
+        row = self.cursor.execute('SELECT * FROM Yonetmen WHERE LOWER(ad_soyad) LIKE ?',
+                                  (f'%{yonetmen_ad_soyad}%',)).fetchone()
         if row:
             return Yonetmen(row[1], row[2])
         return None
 
-    def sifre_guncelle(self, kullanici_adi, yeni_sifre):
+    def sifre_guncelle(self, kullanici_adi, yeni_sifre, guvenlik_cevap):
         # Sadece kullanıcı adını parametre olarak kullanarak kullanıcıyı kontrol et.
         sorgu = 'SELECT * FROM Kullanici WHERE LOWER(kullanici_adi) = ?'
         parametreler = (kullanici_adi.lower(),)  # Tek parametreli bir tuple
         sonuc = self.cursor.execute(sorgu, parametreler).fetchall()
+        sorgu1 = 'SELECT kullanici_adi, sifre FROM Kullanici WHERE kullanici_adi = ? AND guvenlik_sorusu = ?'
+        parametreler = (kullanici_adi, guvenlik_cevap)
+        sonuc1 = self.cursor.execute(sorgu1, parametreler).fetchall()
 
         if sonuc:
-            if yeni_sifre:
-                # Şifre güncelleme sorgusu ve parametreler
-                sorgu = "UPDATE Kullanici SET sifre = ? WHERE LOWER(kullanici_adi) = ?"
-                parametreler = (yeni_sifre, kullanici_adi.lower())
-                self.cursor.execute(sorgu, parametreler)
-                self.connect.commit()
-                print("Şifre başarılı bir şekilde güncellendi.")
+            if sonuc1:
+                if yeni_sifre:
+                    # Şifre güncelleme sorgusu ve parametreler
+                    sorgu = "UPDATE Kullanici SET sifre = ? WHERE LOWER(kullanici_adi) = ?"
+                    parametreler = (yeni_sifre, kullanici_adi.lower())
+                    self.cursor.execute(sorgu, parametreler)
+                    self.connect.commit()
+                    print("Şifre başarılı bir şekilde güncellendi.")
+                else:
+                    print("Yeni şifre boş olamaz.")
+
             else:
-                print("Yeni şifre boş olamaz.")
+                print("Güvenlik sorusunu yanlış cevapladınız.")
         else:
             print("Kullanıcı Bulunamadı.")
 
-
     def kullanici_guncelleme(self, kullanici_adi):
-        row = self.cursor.execute('SELECT * FROM Kullanici WHERE LOWER(kullanici_adi) LIKE ?', (f'%{kullanici_adi}%',)).fetchone()
+        row = self.cursor.execute('SELECT * FROM Kullanici WHERE LOWER(kullanici_adi) LIKE ?',
+                                  (f'%{kullanici_adi}%',)).fetchone()
         if row:
-            kullanici = Kullanici(row[1], row[2], row[3], row[4], row[5])
+            kullanici = Kullanici(row[1], row[2], row[3], row[4], row[5], row[6], row[7])
             kullanici.bilgileri_guncelle()
             self.cursor.execute('''
                 UPDATE Kullanici
                 SET ad = ?, soyad = ?, email = ?, kullanici_adi = ?, sifre = ?
                 WHERE LOWER(kullanici_adi) LIKE ?
-            ''', (kullanici.ad, kullanici.soyad, kullanici.email, kullanici.kullanici_adi, kullanici.sifre, f'%{kullanici_adi}%')).fetchall()
+            ''', (kullanici.ad, kullanici.soyad, kullanici.email, kullanici.kullanici_adi, kullanici.sifre,
+                  f'%{kullanici_adi}%')).fetchall()
             self.connect.commit()
         else:
             print("Kullanıcı bulunamadı.")
 
     def yonetmen_guncelleme(self, yonetmen_ad_soyad):
-        row = self.cursor.execute('SELECT * FROM Yonetmen WHERE LOWER(ad_soyad) LIKE ?', (f'%{yonetmen_ad_soyad}%',)).fetchone()
+        row = self.cursor.execute('SELECT * FROM Yonetmen WHERE LOWER(ad_soyad) LIKE ?',
+                                  (f'%{yonetmen_ad_soyad}%',)).fetchone()
         if row:
             yonetmen = Yonetmen(row[1], row[2])
             yonetmen.bilgileri_guncelle()
@@ -270,7 +314,9 @@ class FilmKutuphanesi:
                 UPDATE Yonetmen
                 SET ad_soyad = ?, dogum_yili = ?
                 WHERE ad_soyad = ?
-            ''', (yonetmen.yonetmen_ad_soyad, yonetmen.dogum_yili, yonetmen_ad_soyad, f'%{yonetmen_ad_soyad}%')).fetchall()
+            ''', (
+                yonetmen.yonetmen_ad_soyad, yonetmen.dogum_yili, yonetmen_ad_soyad,
+                f'%{yonetmen_ad_soyad}%')).fetchall()
             self.connect.commit()
         else:
             print("Yönetmen bulunamadı.")
@@ -278,19 +324,21 @@ class FilmKutuphanesi:
     def film_guncelleme(self, film_adi):
         row = self.cursor.execute('SELECT * FROM Film WHERE LOWER(film_adi) LIKE ?', (f'%{film_adi}%',)).fetchone()
         if row:
-            film = Film(row[1], row[2], row[3], row[4])
+            film = Film(row[1], row[2], row[3], row[4], row[5])
             film.bilgileri_guncelle()
             self.cursor.execute('''
                 UPDATE Film
-                SET film_adi = ?, yonetmen_ad_soyad = ?, cikis_yili = ?, tur = ?
+                SET film_adi = ?, yonetmen_ad_soyad = ?, cikis_yili = ?, tur = ?, sure = ?
                 WHERE film_adi = ?
-            ''', (film.film_adi, film.yonetmen_ad_soyad, film.cikis_yili, film.tur, film_adi, f'%{film_adi}%')).fetchone()
+            ''', (film.film_adi, film.yonetmen_ad_soyad, film.cikis_yili, film.tur, film.sure, film_adi,
+                  f'%{film_adi}%')).fetchone()
             self.connect.commit()
         else:
             print("Film bulunamadı.")
 
     def close_connection(self):
         self.connect.close()
+
 
 film_kutuphanesi = FilmKutuphanesi()
 
@@ -341,8 +389,11 @@ while True:
                     email = input("Email: ")
                     kullanici_adi = input("Kullanıcı Adı: ")
                     sifre = input("Şifre: ")
-                    kullanici = Kullanici(ad, soyad, email, kullanici_adi, sifre)
+                    guvenlik_sorusu = input("Güvenlik Sorusu (Anneniz'in kızlık soyadı'nın son iki hanesini giriniz): ")
+                    cinsiyet = input("Cinsiyet Giriniz: ")
+                    kullanici = Kullanici(ad, soyad, email, kullanici_adi, sifre, guvenlik_sorusu, cinsiyet)
                     film_kutuphanesi.kullanici_ekle(kullanici)
+
                     time.sleep(3)
 
                 elif islem == "2":
@@ -357,11 +408,12 @@ while True:
                     yonetmen_ad_soyad = input("Yönetmen Ad ve Soyad: ")
                     cikis_yili = input("Çıkış Yılı: ")
                     tur = input("Tür: ")
+                    sure = input("Süre (dk.): ")
                     time.sleep(3)
 
                     yonetmen = film_kutuphanesi.yonetmen_aramak(yonetmen_ad_soyad)
                     if yonetmen:
-                        film = Film(film_adi, yonetmen_ad_soyad, cikis_yili, tur)
+                        film = Film(film_adi, yonetmen_ad_soyad, cikis_yili, tur, sure)
                         film_kutuphanesi.film_ekle(film)
                         time.sleep(3)
                     else:
@@ -442,14 +494,17 @@ while True:
         email = input("Email: ")
         kullanici_adi = input("Kullanıcı Adı: ")
         sifre = input("Şifre: ")
-        kullanici = Kullanici(ad, soyad, email, kullanici_adi, sifre)
+        guvenlik_sorusu = input("Güvenlik Sorusu (Anneniz'in kızlık soyadı'nın son iki hanesini giriniz): ")
+        cinsiyet = input("Cinsiyet Giriniz: ")
+        kullanici = Kullanici(ad, soyad, email, kullanici_adi, sifre, guvenlik_sorusu, cinsiyet)
         film_kutuphanesi.kullanici_ekle(kullanici)
         time.sleep(3)
 
     elif islem == "3":
         kullanici_adi = input("Şifresini unuttuğunuz kullanıcı adını giriniz: ")
+        guvenlik_cevap = input("Anneniz'in kızlık soyadının son iki hanesini giriniz: ")
         yeni_sifre = input("Yeni şifre giriniz: ")
-        film_kutuphanesi.sifre_guncelle(kullanici_adi, yeni_sifre)
+        film_kutuphanesi.sifre_guncelle(kullanici_adi, yeni_sifre, guvenlik_cevap)
         time.sleep(3)
 
     elif islem == "4" or islem.lower() == "q":
